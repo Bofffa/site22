4d7a5a8426778b141a7c5969d44b743f6fdb3617
Bofffa <109981350+Bofffa@users.noreply.github.com>
2023-02-20 16:30:05 -0600

    Add files via upload

---
+++ credentials.php
@@ -0,0 +1,5 @@
+<?php
+$name = "Night"; // your application name
+$ownerid = "26ApFoKwBK"; // your KeyAuth account's ownerid, located in account settings
+$SellerKey = "6e2bb1af956b4c26cd83a672473d3620"; // application's seller key. Found in seller settings (You must have seller plan)
+?>
